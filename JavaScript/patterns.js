0d7898f868c402e5d527024e89d0d234cd1c8314
nishanthmaraboina <nishanthmaraboina@gmail.com>
2018-11-10 17:19:00 +0530

    javascript patterns

--- JavaScript/patterns.js
+++ JavaScript/patterns.js
@@ -97,3 +97,148 @@
 //         console.log(count);
 // }
 
+// function print(n){
+//         var star = "*";
+        
+//         for(var i = 1; i <= n;i++){
+//                 var count = "";
+//                 for(var j=1;j<=i;j++){
+//                         count = count + star;
+
+//                 }
+//         console.log(count);
+//         }
+// }
+// print(5);
+
+// print prime numbers from the array
+
+// function nPrime(arr){
+//         for(var i = 0; i < arr.length-1; i++){
+//                 var count = 0;
+//                 for(var j = 1; j <= i+1; j++){
+//                         if(arr[i] % j == 0){
+//                                 count += 1;
+//                         }
+//                 }
+                
+//                 if(count < 3){
+//                         console.log(arr[i]);
+//                 }
+                
+//         }
+// }
+
+
+// nPrime([1,2,3,4,5,6,7,8,9,10]);
+
+/*12344321
+  123**321
+  12****21
+  1******1
+*/
+
+// for(var i = 5; i> 1; i--){
+//   var count = "", star = "*";
+//   for(var j = 1; j<=i;j++){
+//     if(j<i){
+//       count = count + j;
+//     }else{
+//       for(var k = 5; k > j; k --){
+//         count = count + star;
+//       }
+//       count = count + count.split("").reverse().join("");
+//     }
+//   }
+//   console.log(count);
+// }
+
+/*  1
+  2 3 4
+5 6 7 8 9
+*/
+// var num = 0;
+// for(var i = 4; i > 1; i--){
+//   var count = "";
+//   for(var j = 1; j < i-1; j++){
+//     count = count + " ";
+//   }
+//   for(var k = 2*i; k < 9; k ++){
+//     num = num + 1;
+//     count = count + num;
+//   }
+//   console.log(count);
+// }
+//************************************************************************************************ */
+
+//Have to Work on this problem ...Not yet finished....
+// var num = 84;
+// for( var i = 1; i <=4; i ++){
+//   var count = "";
+//   for (var j = 1; j <=4; j++){
+//     num = num + 1;
+//     count = count + num + " ";
+//   }
+//   console.log(count);
+// }
+
+// var num1 = 84;
+// for(var i = 1; i<=4;i++){
+//   var count = "";
+//   if(i==1){
+//     for (var j = 1; j<=4;j++){
+//       num1 = num1 + 1;
+//       count = count + " " + num1;
+//    }
+//   }
+//   if(i==2){
+//     var num2 = 96;
+//     for(var j = 1;j<4;j++){
+//       count = count + " " + num2;
+//     }
+//     num1 = num1 + 1;
+//     count = count + " " + num1;
+//   }
+//   if(i==3){
+//     for(var j = 1;j<4;j++){
+//       count = count + " "+ " "+ " ";
+//     }
+//     num1 = num1 + 1;
+//     count = count + " " + num1;
+//   }
+//   if(i==4){
+//     for(var j = 1;j<4;j++){
+//       count = count + " "+ " "+ " ";
+//     }
+//     num1 = num1 + 1;
+//     count = count + " " + num1;
+//   }
+  
+//    console.log(count);
+
+//   }
+
+
+// var num = 96;
+// for( var i = 1; i <=2; i ++){
+//   var count = "";
+//   for (var j = 1; j <=2; j++){
+//     num = num + 1;
+//     count = count +  " " +num;
+//   }
+//   if(i==2){
+//     count = count.split(" ").reverse().join(" ");
+//   }
+//   console.log(count);
+// }
+
+//*********************************************************************************** */
+
+/*11111
+  2222
+  333
+  22
+  1
+*/
+
+

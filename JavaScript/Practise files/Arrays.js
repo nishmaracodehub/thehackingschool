1d092954902e9e1b7e2a9d40b7ebe9cb6c4767b3
nishanthmaraboina <nishanthmaraboina@gmail.com>
2018-11-21 20:12:17 +0530

    arrays

--- JavaScript/Practise files/Arrays.js
+++ JavaScript/Practise files/Arrays.js
@@ -1,19 +1,7 @@
-//Program for traversing 3 dimensional array
-
-
-// var arr = [[[1,2],[2,3]],[[3,4],[4,5]],[[1,3],[1,2]]];
-// for(var i = 0; i < arr.length;i++){
-//     for(var j = 0; j < arr[i].length;j++){
-//         for(var k = 0; k < arr[j].length; k++){
-//             console.log(arr[i][j][k]);
-//         }
-//     }
-// }
-
 //Extract prime numbers in an Array
 
-var prime1 = [1,2,3,4,7,11,13,15,17,19,23];
-var newPrime = [];
+// var prime1 = [1,2,3,4,7,11,13,15,17,19,23];
+// var newPrime = [];
 //Method 1:
 // function checkPrime(num){
 //     var prime = num != 1;
@@ -78,3 +66,64 @@ var newPrime = [];
 //     return "First Maximum Number " + arr[arr.length-1] + " " + "Second Maximum Number " + arr[arr.length-2];
 // }
 // console.log(maxNum([1,12,6,9,10,4,12,2]));
+
+//Method 3: Not even using Set here to make the array unique
+
+// var maxNum = function (arr){
+//     var first = 0, second = 0;
+//     for(let i = 0; i < arr.length;i++){
+//         if(arr[i] > first){
+//             second = first;
+//             first = arr[i];
+//         }else if(arr[i] > second && arr[i] != first){ //arr[i] != first will eliminate the duplicate value
+//             second = arr[i];
+//         }
+//     }
+//     console.log("first largest:" + " " + first + " second largest:" +" " + second)
+// }
+
+// maxNum([1,12,6,9,10,4,4,2,12,2]);
+//Method 4: Using Apply method(Uses this function, accepts array as Argument)
+
+// var maxNum = function (arr){
+//     arr = Array.from(new Set(arr));
+//     var firstMax = Math.max.apply("",arr);
+//     console.log(firstMax);
+//     arr.splice(arr.indexOf(firstMax),1);
+//     var secondMax = Math.max.apply("",arr);
+//     console.log(secondMax);
+// }
+// maxNum([1,12,6,9,10,4,12,2]);
+
+//Program for three dimensional array
+
+//traversing
+// var arr = [[[1,2],[2,3]],[[3,4],[4,5]],[[1,3],[1,2]]];
+// for(var i = 0; i < arr.length;i++){
+//     for(var j = 0; j < arr[i].length;j++){
+//         for(var k = 0; k < arr[j].length; k++){
+//             console.log(arr[i][j][k]);
+//         }
+//     }
+// }
+
+//Google interview Question
+
+var arr1 = [1,2,3,9];
+var arr2 = [1,2,4,4];
+
+//find a pair of numbers that add up to 8
+function checkFunc(arr){
+    for(var i = 0; i<=arr.length;i++){
+        for(var j = 0;j<arr.length;j++){
+            if(arr[i] + arr[j] == 8){
+                return arr[i] + " " + arr[j];
+            }else{
+                return "No such pairs";
+            }
+        }
+    }
+}
+ 
+console.log(checkFunc([1,2,3,9]));
+

13025729001a9f1a94ab385c7eb0d809983eb4bd
nishanthmaraboina <nishanthmaraboina@gmail.com>
2018-11-26 17:02:31 +0530

    done

---
+++ JavaScript/Practise files/TryCatch.js
@@ -0,0 +1,17 @@
+function reverseString(s) {
+
+        try {
+            s = s.split("").reverse().join("");
+        } 
+        catch (e) {
+            console.log(e.message);
+            
+        }
+        finally {
+            console.log(s);
+        }
+        
+}
+
+reverseString('abcde');
+//reverseString(1234);
